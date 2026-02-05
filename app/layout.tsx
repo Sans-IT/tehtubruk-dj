df549bf946f89dd29abfdeff3e1aaf403baff33d
unknown <steven.sansit@gmail.com>
2026-02-05 14:31:34 +0700

    add meta tag ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,6 +85,8 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
+				
+				<meta name="google-adsense-account" content="ca-pub-4087021836796600"></meta>
 			<Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4087021836796600"
 crossOrigin="anonymous"></Script>
 
